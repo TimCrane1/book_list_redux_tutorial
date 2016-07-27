308e511baf38e51a7a55a5162dafec5ad9743395
Timothy Crane <mickeymouth82@gmail.com>
2016-07-27 11:03:57 -0500

    reducer books

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
-	return [
-		{title: 'Javascript: The Good Parts'},
-		{title: 'Harry Potter'},
-		{title: 'The DarkTower'},
-		{title: 'Eloquent Ruby'},
-	]
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Harry Potter'},
+    {title: 'The DarkTower'},
+    {title: 'Eloquent Ruby'}
+  ]
 }
